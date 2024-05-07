8a7019ab343faa3dab3ca1ddc45132a1247dfb73
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-05-07 11:25:25 +0100

    Add astro compress and ssr to server

--- server/astro.config.mjs
+++ server/astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [(await import("@playform/compress")).default()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
